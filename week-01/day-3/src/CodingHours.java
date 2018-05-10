67d5f5675c9a5690f34534f86bbe2069d1b2cc3a
evo <maraevelin94@gmail.com>
2018-05-10 13:32:17 +0200

    First two parts are done

---
+++ week-01/day-3/src/CodingHours.java
@@ -0,0 +1,23 @@
+public class CodingHours {
+
+
+
+    public static void main(String[] args) {
+        // An average Green Fox attendee codes 6 hours daily
+        // The semester is 17 weeks long
+        //
+        // Print how many hours is spent with coding in a semester by an attendee,
+        // if the attendee only codes on workdays.
+        //
+        // Print the percentage of the coding hours in the semester if the average
+        // work hours weekly is 52
+
+        double dailyCoding = 6;
+        int semesterLength = 17;
+        int workDaysWeekly = 5;
+        int averageWorkHoursWeekly = 52;
+
+        System.out.println("An attende spends " + dailyCoding * workDaysWeekly * semesterLength + " hours with coding");
+        System.out.println("The percentage of the coding hours in the semester is " + dailyCoding * workDaysWeekly / averageWorkHoursWeekly * 100 + " %");
+    }
+}
