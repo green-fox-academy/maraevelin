67c0c25cf5e14d58606baec37f3500048c89c20b
evo <maraevelin94@gmail.com>
2018-05-10 23:13:03 +0200

    FizzBuzz

---
+++ week-01/day-3/src/IWontCheatOnTheExams.java
@@ -0,0 +1,9 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 100) {
+            System.out.println( (a + 1) + ". I won't cheat on the exams");
+            a += 1;
+        }
+    }
+}
---
+++ week-01/day-3/src/PrintEven.java
@@ -0,0 +1,11 @@
+// Create a program that prints all the even numbers between 0 and 500
+
+public class PrintEven {
+    public static void main(String[] args) {
+        int a = 2;
+        while (a < 500) {
+            System.out.println(a);
+            a += 2;
+        }
+    }
+}
