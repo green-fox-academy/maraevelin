1ff4529bdf7ac9e49ec17f9c98efe99297b91dac
evo <maraevelin94@gmail.com>
2018-05-10 12:51:04 +0200

    Starting the exercises

---
+++ week-01/day-3/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String textToPrint = "Hello Seagal";
+        boolean isItTrue = true;
+
+        System.out.println(textToPrint);
+    }
+}
