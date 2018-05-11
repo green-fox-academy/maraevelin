8145e03b2cb136b283920dbd02bdb17f4fa1f197
evo <maraevelin94@gmail.com>
2018-05-11 10:22:22 +0200

    FizzBuzz done

--- week-01/day-3/src/FizzBuzz.java
+++ week-01/day-3/src/FizzBuzz.java
@@ -4,3 +4,19 @@
 // For numbers which are multiples of both three and five print “FizzBuzz”.
 
 
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int numberSequence = 1; numberSequence < 101; numberSequence++ ) {
+            if (numberSequence % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (numberSequence % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (numberSequence % 3 == 0) {
+                System.out.println("Fizz");
+            }else {
+                    System.out.println(numberSequence);
+                }
+            }
+            }
+        }
