e4426c5d1df067a7d28c258f33395295cf4bdfca
evo <maraevelin94@gmail.com>
2018-05-11 13:19:59 +0200

    Stuck at Diagonal

---
+++ week-01/day-3/src/DrawDiagonal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DrawDiagonal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("To draw a square, enter a number:");
+
+        int userInput1 = scanner.nextInt();
+        
+
+        ???
+    }
+}
