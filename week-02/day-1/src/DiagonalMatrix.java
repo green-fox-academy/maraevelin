ed92535241802d734349e718f1baebb3dd201c73
evo <maraevelin94@gmail.com>
2018-05-14 17:40:17 +0200

    diagonal Matrix

--- week-02/day-1/src/DiagonalMatrix.java
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -16,20 +16,22 @@ public class DiagonalMatrix {
   int dynamicValue = 4;
   int[][] arraysOfMatrix = new int[dynamicValue][dynamicValue];
 
-for (int row = 0; row < arraysOfMatrix.length; row++) {
-  for (int column = 0; column < arraysOfMatrix.length; column++) {
-    if (row == column) {
-      arraysOfMatrix[row][column] = 1;
-    } else {
-      arraysOfMatrix[row][column] = 0;
-    }
+   for (int row = 0; row < arraysOfMatrix.length; row++) {
+    for (int column = 0; column < arraysOfMatrix.length; column++) {
+      if (row == column) {
+        arraysOfMatrix[row][column] = 1;
+      } else {
+        arraysOfMatrix[row][column] = 0;
+      }
 
+     }
 
-  }
-
-
-}
-    System.out.println(Arrays.deepToString(arraysOfMatrix));
+   }
+   int newRow = 0;
+   for (int[] array : arraysOfMatrix) {
+     System.out.println(Arrays.toString(arraysOfMatrix[newRow]));
+     newRow++;
+   }
 
   }
 
