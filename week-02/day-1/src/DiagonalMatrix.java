f240f39bc03b3cbc023e2248ce5f9c47c3e2ebbc
evo <maraevelin94@gmail.com>
2018-05-14 17:28:58 +0200

    almost a matrix

---
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -0,0 +1,37 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+
+import java.util.Arrays;
+
+public class DiagonalMatrix {
+  public static void main(String[] args) {
+
+  int dynamicValue = 4;
+  int[][] arraysOfMatrix = new int[dynamicValue][dynamicValue];
+
+for (int row = 0; row < arraysOfMatrix.length; row++) {
+  for (int column = 0; column < arraysOfMatrix.length; column++) {
+    if (row == column) {
+      arraysOfMatrix[row][column] = 1;
+    } else {
+      arraysOfMatrix[row][column] = 0;
+    }
+
+
+  }
+
+
+}
+    System.out.println(Arrays.deepToString(arraysOfMatrix));
+
+  }
+
+}
+
