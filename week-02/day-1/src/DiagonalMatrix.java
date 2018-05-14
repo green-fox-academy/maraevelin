32a570f25766d7d59cf4df13c5c64c91567e6f58
evo <maraevelin94@gmail.com>
2018-05-14 17:46:24 +0200

    diagnotMatrix

--- week-02/day-1/src/DiagonalMatrix.java
+++ week-02/day-1/src/DiagonalMatrix.java
@@ -13,27 +13,39 @@ import java.util.Arrays;
 public class DiagonalMatrix {
   public static void main(String[] args) {
 
-  int dynamicValue = 4;
-  int[][] arraysOfMatrix = new int[dynamicValue][dynamicValue];
+    int dynamicValue = 4;
+    int[][] arraysOfMatrix = new int[dynamicValue][dynamicValue];
 
-   for (int row = 0; row < arraysOfMatrix.length; row++) {
-    for (int column = 0; column < arraysOfMatrix.length; column++) {
-      if (row == column) {
-        arraysOfMatrix[row][column] = 1;
-      } else {
-        arraysOfMatrix[row][column] = 0;
+    for (int row = 0; row < arraysOfMatrix.length; row++) {
+      for (int column = 0; column < arraysOfMatrix.length; column++) {
+        if (row == column) {
+          arraysOfMatrix[row][column] = 1;
+        } else {
+          arraysOfMatrix[row][column] = 0;
+        }
+
       }
 
-     }
+    }
+//    System.out.println(Arrays.deepToString(arraysOfMatrix));
+// }
+//}
 
-   }
-   int newRow = 0;
-   for (int[] array : arraysOfMatrix) {
-     System.out.println(Arrays.toString(arraysOfMatrix[newRow]));
-     newRow++;
-   }
-
+    int newRow = 0;
+    for (int[] array : arraysOfMatrix) {
+      System.out.println(Arrays.toString(arraysOfMatrix[newRow]));
+      newRow++;
+    }
   }
-
 }
+
+
+
+
+
+
+
+
+
+
 
