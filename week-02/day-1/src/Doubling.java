7e3878d61977f15b756ddfb4ae2a2302482916df
evo <maraevelin94@gmail.com>
2018-05-14 15:01:23 +0200

    appenda function

--- week-02/day-1/src/Doubling.java
+++ week-02/day-1/src/Doubling.java
@@ -6,16 +6,16 @@ public class Doubling {
   public static void main(String[] args) {
     int baseNum = 123;
     System.out.println(doubling(baseNum));
-    }
+  }
 
-    public static int doubling(int number) {
+  public static int doubling(int number) {
     //'int' must be used for 'return-type'
     // 'number' can be written, 'sout' in the main section will substitute it with the relevant element
     // 'void functions' can be used with souts (<=> 'return' functions)
 
-      int doubleNum = (number *= 2);
-      return doubleNum;
-
-    }
+    int doubleNum = (number *= 2);
+    return doubleNum;
 
   }
+
+}
---
+++ week-02/day-1/src/Sum.java
@@ -0,0 +1,16 @@
+////  Create the usual class wrapper
+////  and main method on your own.
+//
+//// - Write a function called `sum` that sum all the numbers
+////   until the given parameter and returns with an integer
+//
+//public class Sum {
+//  public static void main(String[] args) {
+//    int parameterInput = 4;
+//    System.out.println(sum(parameterInput));
+//  }
+//
+//
+//
+//
+//
