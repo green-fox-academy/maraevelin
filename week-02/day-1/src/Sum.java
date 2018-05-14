8dd7850942810a82fb06e2f0c5028a65ad98d3db
evo <maraevelin94@gmail.com>
2018-05-14 15:13:57 +0200

    Sum function

--- week-02/day-1/src/Sum.java
+++ week-02/day-1/src/Sum.java
@@ -4,13 +4,17 @@
 //// - Write a function called `sum` that sum all the numbers
 ////   until the given parameter and returns with an integer
 //
-//public class Sum {
-//  public static void main(String[] args) {
-//    int parameterInput = 4;
-//    System.out.println(sum(parameterInput));
-//  }
-//
-//
-//
-//
-//
+public class Sum {
+
+  public static void main(String[] args) {
+    int givenParamter = 5;
+    System.out.println(sum(givenParamter));
+  }
+
+
+  public static int sum(int maxValue) {
+    int sumValue = maxValue * (maxValue + 1) / 2;
+    return sumValue;
+
+  }
+}
