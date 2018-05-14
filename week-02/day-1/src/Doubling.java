b07019f135fde1c5aa3ef553412fa8904dfb7cf9
evo <maraevelin94@gmail.com>
2018-05-14 13:12:24 +0200

    doubling

---
+++ week-02/day-1/src/Doubling.java
@@ -0,0 +1,21 @@
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
+
+public class Doubling {
+  public static void main(String[] args) {
+    int baseNum = 123;
+    System.out.println(doubling(baseNum));
+    }
+
+    public static int doubling(int number) {
+    //'int' must be used for 'return-type'
+    // 'number' can be written, 'sout' in the main section will substitute it with the relevant element
+    // 'void functions' can be used with souts (<=> 'return' functions)
+
+      int doubleNum = (number *= 2);
+      return doubleNum;
+
+    }
+
+  }
---
+++ week-02/day-1/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    greetFunction();
+  }
+
+  public static void greetFunction() {
+    // Prints "Hello Green Fox!" to the terminal window.
+    System.out.println("Hello Green Fox!");
+  }
+
+}
