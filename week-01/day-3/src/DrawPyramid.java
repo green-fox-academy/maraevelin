00b8209f135b8949fef9ddae7faca13e3c7cc476
evo <maraevelin94@gmail.com>
2018-05-14 09:22:26 +0200

    Stuck at pyramid

--- week-01/day-3/src/DrawPyramid.java
+++ week-01/day-3/src/DrawPyramid.java
@@ -1,1 +1,23 @@
-//
+//// Write a program that reads a number from the standard input, then draws a
+//// pyramid like this:
+////
+////
+////    *
+////   ***
+////  *****
+//// *******
+////
+//// The pyramid should have as many lines as the number was
+
+ import java.util.Scanner;
+ public class DrawPyramid {
+
+     public static void main(String[] args) {
+
+         System.out.println("You will draw a pyramid. Please enter an integer number to give its height:");
+         Scanner scanner = new Scanner(System.in);
+
+
+
+     }
+ }
