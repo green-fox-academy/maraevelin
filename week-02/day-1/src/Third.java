b55743eaa4eda5502935e088a3c7171def5c9cac
evo <maraevelin94@gmail.com>
2018-05-14 16:20:19 +0200

    sumElements

---
+++ week-02/day-1/src/Third.java
@@ -0,0 +1,26 @@
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+//public class Arrays {
+//  public static void main(String[] args) {
+//    int[] myList = {1, 2, 3, 6};
+//
+//    // Print the second element of the array
+//    System.out.println("The second element is " + myList[1]);
+//
+//    // Print all the array elements
+//    for (int i = 0; i < myList.length; i++) {
+//      System.out.println(myList[i] + " ");
+//    }
+//  }
+//}
+
+public class Third {
+  public static void main(String[] args) {
+
+    int[] q = {4, 5, 6, 7};
+
+    System.out.println("The third element is " + q[2]);
+  }
+}
