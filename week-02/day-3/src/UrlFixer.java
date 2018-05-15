cb5e4d3e6edd021edbd401ed92d3a1586544ac1f
evo <maraevelin94@gmail.com>
2018-05-15 12:13:06 +0200

    urlPerfection

---
+++ week-02/day-3/src/UrlFixer.java
@@ -0,0 +1,42 @@
+//public class UrlFixer {
+//  public static void main(String... args){
+//    String url = "https//www.reddit.com/r/nevertellmethebots";
+//    https://www.reddit.com/r/nevertellmetheodds
+//
+//    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+//    // Also, the URL is missing a crutial component, find out what it is and insert it too!
+//    // Try to solve it more than once using different String functions!
+//
+//    System.out.println(url);
+//  }
+//}
+
+public class UrlFixer {
+  public static void main(String[] args) {
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+
+    String missingPart = "https:";
+    String fixedPartShort = "odds";
+    String fixedPartLong = "nevertellmetheodds";
+
+
+    String urlFixedTypeA = url.replace("https", missingPart);
+    url = urlFixedTypeA;
+    urlFixedTypeA = url.replace("bots", fixedPartShort);
+    url = urlFixedTypeA;
+
+    System.out.println(url);
+
+    System.out.println(missingPart + url.substring(url.indexOf("/"), url.indexOf("n")) + fixedPartLong);
+
+    String urlPerfection =  missingPart + url.substring(url.indexOf("/"), url.indexOf("n")) + fixedPartLong;
+
+    System.out.println(urlPerfection);
+
+
+
+
+
+  }
+
+}
