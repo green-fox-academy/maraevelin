f4167b19df3c563981c72fff7799524805d7495a
evo <maraevelin94@gmail.com>
2018-05-15 08:53:42 +0200

    Hard Ones are coming

---
+++ week-02/day-1/src/Anagram.java
@@ -0,0 +1,8 @@
+// Create a function named is anagram
+// following your current language's style guide.
+// It should take two strings and return
+// a boolean value depending on whether
+// its an anagram or not.
+
+
+
