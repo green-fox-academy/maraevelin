2268a391c94a855e0e34a4a1051b99104e4bf7ae
evo <maraevelin94@gmail.com>
2018-05-16 14:06:58 +0200

    write single line

---
+++ week-02/day-3/src/WriteSingleLine.java
@@ -0,0 +1,20 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+  public static void main(String[] args) {
+    List<String> contentIsMyName = new ArrayList<>();
+    contentIsMyName.add("Mára Evelin");
+
+    try {
+      Path filePath = Paths.get("./my-file.txt");
+      Files.write(filePath, contentIsMyName);
+    } catch (Exception e) {
+      System.out.println("Unable to write file: my-file.txt");
+    }
+
+  }
+}
