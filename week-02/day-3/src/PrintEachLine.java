3246ca7229ea0092c9f29957e1ea8fa1c0bd34eb
evo <maraevelin94@gmail.com>
2018-05-16 13:13:50 +0200

    PrintEachLine

---
+++ week-02/day-3/src/PrintEachLine.java
@@ -0,0 +1,41 @@
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.Files;
+//
+//public class ReadFile {
+//  public static void main(String[] args) {
+//    try { // Required by Files.readAllLines(filePath);
+//      // Reads the content from `lorem-psum.txt` in the `assets` folder line by line to a String List
+//      Path filePath = Paths.get("assets/lorem-ipsum.txt");
+//      List<String> lines = Files.readAllLines(filePath);
+//      System.out.println(lines.get(0)); // Prints the first line of the file
+//    } catch (Exception e) {
+//      System.out.println("Uh-oh, could not read the file!");
+//    }
+//  }
+//}
+
+
+// Write a program that opens a file called "my-file.txt", then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: "Unable to read file: my-file.txt"
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
+public class PrintEachLine {
+  public static void main (String[] args) {
+    try {
+      Path filePath = Paths.get("my-file.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      System.out.println(lines.get(0));
+
+    } catch (Exception e) {
+      System.out.println("Unable to read file: my-file.txt");
+    }
+
+    }
+}
