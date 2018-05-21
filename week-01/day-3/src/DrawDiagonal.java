9732fb3e7e6e07e5011b94c0d36e2d17605c9322
evo <maraevelin94@gmail.com>
2018-05-21 11:12:26 +0200

    draw Diagonal

--- week-01/day-3/src/DrawDiagonal.java
+++ week-01/day-3/src/DrawDiagonal.java
@@ -1,1 +1,33 @@
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
 //
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %%%%%
+//
+// The square should have as many lines as the number was// Write a program that reads a number from the standard input, then draws a
+//// square like this:
+
+import java.util.Scanner;
+
+public class DrawDiagonal {
+  public static void main(String[] args) {
+    int standardInput = 5;
+    String sketch = "% ";
+    String emptySpace = "  ";
+
+    for (int i = 0; i < standardInput; i++) {
+      System.out.println(emptySpace);
+      for (int j = 0; j < standardInput; j++) {
+        if (i == 0 || i == j || i == standardInput - 1 || j == 0 || j == standardInput - 1) {
+          System.out.print(sketch);
+        } else {
+          System.out.print(emptySpace);
+        }
+      }
+    }
+  }
+}
