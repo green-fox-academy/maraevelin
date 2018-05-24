e671a8c429e8e2da7dd7501c00a63d4471dfb978
evo <maraevelin94@gmail.com>
2018-05-24 18:30:09 +0200

    string 3 for tomorrow

--- week-03/day-4/src/StringsAgainAgain.java
+++ week-03/day-4/src/StringsAgainAgain.java
@@ -1,1 +1,20 @@
-//// Given a string, compute recursively a new string where all the
+// Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a "*".
+
+public class StringsAgainAgain {
+  public static void main(String[] args) {
+    String original = "xsxdxfxg";
+    String newString = function(original);
+    System.out.println(newString);
+  }
+
+  private static String function(String original) {
+    if (original.length() == 0) {
+      return "";
+    } else {
+      return
+          original.substring(0,1)
+          + "*" + function(original.substring(1));
+    }
+  }
+}
