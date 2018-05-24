517faf6fe44887a39fed64b41aba8b6903516b0b
evo <maraevelin94@gmail.com>
2018-05-24 17:47:24 +0200

    StringFirst: corrected(longliveBarni)

--- week-03/day-4/src/StringXsToYs.java
+++ week-03/day-4/src/StringXsToYs.java
@@ -3,6 +3,21 @@
 
 public class StringXsToYs {
   public static void main(String[] args) {
-    String original = "xsxdxf";
-    System.out.println(original.substring(1, original.length()));
+    String original = "xsxdxfxg";
+    String newString = function(original);
+    System.out.println(newString);
+  }
 
+  private static String function(String original) {
+    System.out.println(original);
+    if (original.length() == 0) {
+      return "";
+    } else {
+      if (original.substring(0,1).equals("x")) {
+        return "y" + function(original.substring(1));
+      } else {
+        return original.substring(0,1) + function(original.substring(1));
+      }
+    }
+  }
+}
