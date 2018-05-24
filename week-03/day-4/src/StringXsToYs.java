fa6b30eac7f99a40c9dbe697941d47fe8574d2f7
evo <maraevelin94@gmail.com>
2018-05-24 14:13:13 +0200

    Strings Again: done

--- week-03/day-4/src/StringXsToYs.java
+++ week-03/day-4/src/StringXsToYs.java
@@ -1,7 +1,7 @@
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
 
-public class String1 {
+public class StringXsToYs {
   public static void main(String[] args) {
     String yToX = yForEveryone("xsxdxf");
     System.out.println(yToX);
---
+++ week-03/day-4/src/StringsAgain.java
@@ -0,0 +1,17 @@
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+public class StringsAgain {
+  public static void main(String[] args) {
+    String givenString = "xsxdxfxgxh";
+    String stringWithoutX =noX(givenString);
+    System.out.println(stringWithoutX);
+  }
+
+  private static String noX(String givenString) {
+  if (givenString.split("x").equals(givenString)) {
+    return givenString;
+  } else {
+    return givenString.replace("x", "");
+  }
+  }
+}
