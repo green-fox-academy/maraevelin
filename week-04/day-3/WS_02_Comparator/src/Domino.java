4b037d7dc4ec6b69038dede161f8999cd2887e0c
evo <maraevelin94@gmail.com>
2018-05-30 12:55:33 +0200

    feat(comparator): DominoPartDone

--- week-04/day-3/WS_02_Comparator/src/Domino.java
+++ week-04/day-3/WS_02_Comparator/src/Domino.java
@@ -1,2 +1,24 @@
-public class Domino {
+import java.util.Collections;
+
+public class Domino implements Comparable<Domino> {
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  @Override
+  public int compareTo(Domino o) {
+    return toString().compareTo(o.toString());
+  }
+
 }
---
+++ week-04/day-3/WS_02_Comparator/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+
+    Collections.sort(dominoes, new Comparator<Domino>() {
+      @Override
+      public int compare(Domino o1, Domino o2) {
+          return o1.compareTo(o2);
+        //        return o1.compareTo(o2);
+      }
+    });
+    System.out.println(Arrays.asList(dominoes));
+  }
+}
