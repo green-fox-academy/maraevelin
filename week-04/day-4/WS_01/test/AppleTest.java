7ad9291a21cc0e3eedb326139ae3457c29385333
evo <maraevelin94@gmail.com>
2018-05-31 12:02:20 +0200

    feat(Apple): testOneWorking

--- week-04/day-4/WS_01/test/AppleTest.java
+++ week-04/day-4/WS_01/test/AppleTest.java
@@ -3,5 +3,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 public class AppleTest {
-  
+
+  static Apple apple;
+
+  @BeforeClass
+  public static void init() {
+    apple = new Apple();
+  }
+
+  @Test
+  public void should_returnString() {
+    String expectedResult = "apple";
+
+    String result = apple.getApple();
+
+    assertEquals(expectedResult, result);
+
+  }
+
 }
