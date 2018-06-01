883bf2876965cfb70837bfaaf638006fc391e89c
evo <maraevelin94@gmail.com>
2018-06-01 13:57:39 +0200

    feat(cab): shouldGiveGuessResult

--- week-04/day-4/WS_01/src/cowsandbulls/CAB.java
+++ week-04/day-4/WS_01/src/cowsandbulls/CAB.java
@@ -45,9 +45,18 @@ public class CAB {
   }
 
   private String giveGuessResult(ArrayList<Integer> beGuessed, ArrayList<Integer> actualGuess) {
-    
-    return "";
+    String guessResult = "";
+    for (int i = 0; i < 4; i++) {
+      int temp = actualGuess.get(i);
+      if (temp == beGuessed.get(i)) {
+        guessResult = guessResult + "cow";
+      } else if (beGuessed.contains(temp)) {
+        guessResult = guessResult + "bull";
+      } guessResult = guessResult + "none";
+    }
+    return guessResult;
   }
+
 
   private void increaseGuessCounter() {
     this.guessCounter++;
