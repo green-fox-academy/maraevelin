ec533a8ae111dbbf71db72ac4d2b57efbe20a1b9
evo <maraevelin94@gmail.com>
2018-06-18 12:46:35 +0200

    refact(FirstALastIFinder): initAndDone

--- week-06/day-01/src/E06FirstALastIFinder.java
+++ week-06/day-01/src/E06FirstALastIFinder.java
@@ -6,6 +6,13 @@ public class E06FirstALastIFinder {
   public static void main(String[] args) {
     ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
     getFirstLetterALastLetterI(cities);
+    getStartsWithAndEndsWith(cities);
+  }
+
+  private static void getStartsWithAndEndsWith(ArrayList<String> cities) {
+    cities.stream()
+        .filter(city -> city.startsWith("A") && city.endsWith("I"))
+        .forEach(System.out::println);
   }
 
   private static void getFirstLetterALastLetterI(ArrayList<String> cities) {
---
+++ week-06/day-01/src/E07CharFrequencyFinder.java
@@ -0,0 +1,14 @@
+//public class E07CharFrequencyFinder {
+//
+//  public static void main(String[] args) {
+//
+//    String example = "Write a Stream Expression to find the frequency of characters in a given string!";
+//    getCharFrequency(example);
+//  }
+//
+//  private static void getCharFrequency(String example) {
+//    example.chars()
+//
+//  }
+//
+//}
