f8a93e28cbd9818fc61c8493b1b46dedce96da62
evo <maraevelin94@gmail.com>
2018-06-18 15:06:30 +0200

    feat(Foxes): initAndDone

---
+++ week-06/day-01/src/E10OO/Fox.java
@@ -0,0 +1,26 @@
+package E10OO;
+
+public class Fox {
+  private String name;
+  private String type;
+  private String colour;
+
+  Fox(String name, String type, String colour) {
+    this.name = name;
+    this.type = type;
+    this.colour = colour;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getColour() {
+    return colour;
+  }
+
+}
---
+++ week-06/day-01/src/E10OO/Main.java
@@ -0,0 +1,38 @@
+package E10OO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+  public static void main(String[] args) {
+    List<Fox> foxes = new ArrayList<>();
+    foxes.add(new Fox("Franci", "Pallida", "black"));
+    foxes.add(new Fox("Juci", "NotPallida", "yellow"));
+    foxes.add(new Fox("Dani", "Pallida", "green"));
+    foxes.add(new Fox("Attila", "NotPallida", "red"));
+    foxes.add(new Fox("Peti", "Pallida", "blue"));
+
+    System.out.println(findPallida(foxes));
+    System.out.println(findPallidaAndGreen(foxes));
+    ;
+  }
+
+  private static String findPallida(List<Fox> foxes) {
+    return foxes.stream()
+        .filter(fox -> fox.getType().equalsIgnoreCase("pallida"))
+        .map(fox -> fox.getName().toString())
+        .collect(Collectors.joining(", "));
+
+
+  }
+
+  private static String findPallidaAndGreen(List<Fox> foxes) {
+    return foxes.stream()
+        .filter(fox -> fox.getType().equalsIgnoreCase("pallida") && fox.getColour().equalsIgnoreCase("green"))
+        .map(fox -> fox.getName().toString())
+        .collect(Collectors.joining(", "));
+
+  }
+}
