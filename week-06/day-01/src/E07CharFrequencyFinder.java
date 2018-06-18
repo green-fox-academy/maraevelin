82fbc9175cd8419a49d3f547e8acf872910d7e03
evo <maraevelin94@gmail.com>
2018-06-18 13:31:32 +0200

    feat(CharFrequency): initDone

--- week-06/day-01/src/E07CharFrequencyFinder.java
+++ week-06/day-01/src/E07CharFrequencyFinder.java
@@ -1,14 +1,19 @@
-//public class E07CharFrequencyFinder {
-//
-//  public static void main(String[] args) {
-//
-//    String example = "Write a Stream Expression to find the frequency of characters in a given string!";
-//    getCharFrequency(example);
-//  }
-//
-//  private static void getCharFrequency(String example) {
-//    example.chars()
-//
-//  }
-//
-//}
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class E07CharFrequencyFinder {
+
+  public static void main(String[] args) {
+
+    String example = "Write a Stream Expression to find the frequency of characters in a given string!";
+    System.out.println((getCharFrequency(example)));
+  }
+
+  private static Map<Character, Integer> getCharFrequency(String example) {
+    return example.chars()
+        .mapToObj(character -> (char) character)
+        .collect(Collectors.toConcurrentMap(character -> character, character -> 1, Integer::sum));
+  }
+
+}
