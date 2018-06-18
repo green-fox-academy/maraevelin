c75379a2e485bf036c3c9117ef2fa511f5a30b5c
evo <maraevelin94@gmail.com>
2018-06-18 11:39:28 +0200

    feat(OddsAverage) initAndDone

--- week-06/day-01/src/E02OddsAverage.java
+++ week-06/day-01/src/E02OddsAverage.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class E01EvenNumbers {
+public class E02OddsAverage {
 
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
-    System.out.println(getEvenNumbers(numbers));
+    System.out.println(getOddsAverage(numbers));
   }
 
-  private static List<Integer> getEvenNumbers(ArrayList<Integer> numbers) {
+  private static double getOddsAverage(ArrayList<Integer> numbers) {
     return numbers.stream()
-        .filter(number -> number % 2 == 0)
-        .collect(Collectors.toList());
+        .filter(number -> number % 2 != 0)
+        .mapToInt(number -> number)
+        .average()
+        .getAsDouble();
   }
 
 }
