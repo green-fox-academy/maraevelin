865e7736f38e21158231774d18d9d8294145e7fc
evo <maraevelin94@gmail.com>
2018-06-25 15:12:07 +0200

    feat(Spring): AtomicLong

--- week-07/day-01/spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/day-01/spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Greeting {
   long id;
   String content;
--- week-07/day-01/spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,16 +1,19 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
 
+  private AtomicLong idCounter = new AtomicLong();
+
   @RequestMapping
   public Greeting greeting(@RequestParam(value = "name") String name) {
-    return new Greeting(1, "Hello, " + name);
+    return new Greeting(idCounter.getAndIncrement(), "Hello, " + name);
     }
 
 }
