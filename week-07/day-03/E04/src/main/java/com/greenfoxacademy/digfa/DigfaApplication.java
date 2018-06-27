4e320faa90dc1e459d0827c6d41d009064bcd7d3
evo <maraevelin94@gmail.com>
2018-06-27 18:13:42 +0200

    refact(HelloBean): better html

---
+++ week-07/day-03/E04/src/main/java/com/greenfoxacademy/digfa/DigfaApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.digfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigfaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DigfaApplication.class, args);
+  }
+}
---
+++ week-07/day-03/E04/src/test/java/com/greenfoxacademy/digfa/DigfaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.digfa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DigfaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
