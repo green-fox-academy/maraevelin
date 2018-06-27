0572de12f92b433b1bb6c900882f2c614adab673
evo <maraevelin94@gmail.com>
2018-06-27 17:12:12 +0200

    feat(HelloBean): correct boolean()

--- week-07/day-03/E01/src/main/java/com/greenfoxacademy/dependency/useful_utilities_controllers/UtilityService.java
+++ week-07/day-03/E01/src/main/java/com/greenfoxacademy/dependency/useful_utilities_controllers/UtilityService.java
@@ -26,11 +26,7 @@ public class UtilityService {
   }
 
   public boolean validateEmail(String email) {
-    if (email.contains("@") && email.contains(".")) {
-      return true;
-    } else {
-      return false;
-    }
+    return (email.contains("@") && email.contains("."));
   }
 
   public String caesar(String text, int number) {
