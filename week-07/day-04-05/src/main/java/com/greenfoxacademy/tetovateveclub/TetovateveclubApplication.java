68b84f35815385c94c3ed1d8954235b2df6ad0d4
evo <maraevelin94@gmail.com>
2018-06-28 13:14:01 +0200

    feat(Project) init

---
+++ week-07/day-04-05/src/main/java/com/greenfoxacademy/tetovateveclub/TetovateveclubApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.tetovateveclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetovateveclubApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(TetovateveclubApplication.class, args);
+  }
+}
