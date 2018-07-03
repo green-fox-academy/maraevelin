bdb5c7bdf4022de811a9fc71dfed8eed14168cda
evo <maraevelin94@gmail.com>
2018-07-03 13:42:15 +0200

    feat(Todo): @Autowired repo

--- week-08/day-02/src/main/java/com/greenfoxacademy/todospring/controllers/TodoController.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/todospring/controllers/TodoController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.todospring.controllers;
 
+import com.greenfoxacademy.todospring.repo.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
+
+  @Autowired
+  TodoRepository todoRepository;
 
   @GetMapping(value={"/", "/list"})
   @ResponseBody
