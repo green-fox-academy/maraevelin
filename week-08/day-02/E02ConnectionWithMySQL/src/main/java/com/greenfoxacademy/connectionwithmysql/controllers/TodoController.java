a260a3609c1943ffdbb1db427221a59e684f49d3
evo <maraevelin94@gmail.com>
2018-07-03 19:40:49 +0200

    feat(ActiveTodos) url is not completely right

--- week-08/day-02/E02ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
+++ week-08/day-02/E02ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
@@ -1,12 +1,12 @@
 package com.greenfoxacademy.connectionwithmysql.controllers;
 
-
 import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/todo")
@@ -19,6 +19,12 @@ public class TodoController {
   public String list(Model model) {
     model.addAttribute("todos", todoRepository.findAll());
     return "todoslist";
+  }
+
+  @GetMapping("/actives")
+  public String listActiveTodos(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "activetodos";
   }
 
 }
