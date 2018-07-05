d2093d37beb0df6af54c55ab0c35181cc759c00b
evo <maraevelin94@gmail.com>
2018-07-05 15:37:23 +0200

    feat(TodoApp) restart

--- week-08/day-04/todoApp/src/main/java/com/greenfoxacademy/connectionwithmysql/repositories/AssigneeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy.connectionwithmysql.repositories;
-
-import com.greenfoxacademy.connectionwithmysql.models.Assignee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssigneeRepository extends CrudRepository<Assignee, Long> {
-}
