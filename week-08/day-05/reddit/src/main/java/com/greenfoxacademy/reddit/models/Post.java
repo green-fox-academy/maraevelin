365d2981e507c797e6f851a95cb49f34ccf45498
evo <maraevelin94@gmail.com>
2018-07-06 09:43:28 +0200

    feat(PostClass): setup

---
+++ week-08/day-05/reddit/src/main/java/com/greenfoxacademy/reddit/models/Post.java
@@ -0,0 +1,32 @@
+package com.greenfoxacademy.reddit.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private int like = 0;
+  private String title;
+  private String url;
+
+  public Post() {
+
+  }
+
+  public Post(String title, String url) {
+    this.title = title;
+    this.url = url;
+  }
+
+}
