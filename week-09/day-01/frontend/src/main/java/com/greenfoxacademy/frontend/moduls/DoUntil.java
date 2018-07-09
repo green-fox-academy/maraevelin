2879bf9b99442a0b9669c1d091f7ddbc0bd6fc88
evo <maraevelin94@gmail.com>
2018-07-09 16:12:15 +0200

    feat(DoUntil): done with recursion

--- week-09/day-01/frontend/src/main/java/com/greenfoxacademy/frontend/moduls/DoUntil.java
+++ week-09/day-01/frontend/src/main/java/com/greenfoxacademy/frontend/moduls/DoUntil.java
@@ -7,6 +7,7 @@ public class DoUntil {
   }
 
   public DoUntil(String what, int until) {
+    result = 0;
     if (what.equals("sum")) {
       result = getSum(until);
     } else if (what.equals("factor")) {
@@ -18,14 +19,14 @@ public class DoUntil {
     if (until == 1) {
       return 1;
     }
-    return (until * getFactor(until - 1));
+    return until * getFactor(until - 1);
   }
 
   private int getSum(int until) {
     if (until == 0) {
       return 0;
     }
-    return (until + getSum(until - 1));
+    return until + getSum(until - 1);
   }
 
   public int getResult() {
