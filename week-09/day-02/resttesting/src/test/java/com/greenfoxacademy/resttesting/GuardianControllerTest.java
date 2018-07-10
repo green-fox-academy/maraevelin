48deed0268886ffd8d93eedc88efba7300e942b8
evo <maraevelin94@gmail.com>
2018-07-10 15:20:46 +0200

    feat(Groot): missingMessage test

---
+++ week-09/day-02/resttesting/src/test/java/com/greenfoxacademy/resttesting/GuardianControllerTest.java
@@ -0,0 +1,49 @@
+package com.greenfoxacademy.resttesting;
+
+import com.greenfoxacademy.resttesting.controllers.GuardianController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GuardianController.class)
+public class GuardianControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+      MediaType.APPLICATION_JSON.getSubtype(),
+      Charset.forName("utf8"));
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void getGroot_succesfull() throws Exception {
+    mockMvc.perform(get("/groot")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"message\": \"somemessage\"}"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(contentType))
+        .andExpect(jsonPath("$.received", is("somemessage")))
+        .andExpect(jsonPath("$.translated", is("I am Groot!")));
+  }
+
+  @Test
+  public void getGroot_missingMessage() throws Exception {
+    mockMvc.perform(get("/groot")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"message\"}"))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(contentType))
+        .andExpect(jsonPath("$.error", is("I am Groot!")));
+  }
+}
